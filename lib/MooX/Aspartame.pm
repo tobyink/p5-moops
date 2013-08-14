569e8e7474de360f170bd1503360d92aa84d2ef8
Toby Inkster <mail@tobyinkster.co.uk>
2013-08-14 12:57:20 +0100

    bump version; changelog

--- lib/MooX/Aspartame.pm
+++ lib/MooX/Aspartame.pm
@@ -8,7 +8,7 @@ use warnings FATAL => 'all';
 no warnings @crud;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.003';
+our $VERSION   = '0.004';
 
 use B                               qw(perlstring);
 use Carp                            qw(croak);

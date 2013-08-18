b0102c919135c387c5fdc85d6bfddc8be90e4cdc
Toby Inkster <mail@tobyinkster.co.uk>
2013-08-18 21:03:41 +0100

    bump version; changelog

--- lib/Moops/DefineKeyword.pm
+++ lib/Moops/DefineKeyword.pm
@@ -6,7 +6,7 @@ no warnings qw(void once uninitialized numeric);
 package Moops::DefineKeyword;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.007';
+our $VERSION   = '0.008';
 
 sub import
 {

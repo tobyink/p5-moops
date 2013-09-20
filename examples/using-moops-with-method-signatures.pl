ad897827d6d775bd5f0a69577d09f1944a8eadc1
Toby Inkster <mail@tobyinkster.co.uk>
2013-09-20 16:50:14 +0100

    Method::Signatures example

---
+++ examples/using-moops-with-method-signatures.pl
@@ -0,0 +1,19 @@
+use Moops;
+ 
+role Moops::TraitFor::Keyword::MS {
+	around generate_package_setup () {
+		return map {
+			/\Ause Function::Parameters/ ? 'use Method::Signatures;' : $_
+		} $self->${^NEXT}, q[use experimental "smartmatch";];
+	}
+}
+ 
+# Now this class uses Method::Signatures!
+class Foo :MS {	
+	method foo ( Int $score where [0..10] ) {
+		say "Got: $score";
+	}
+}
+ 
+Foo->foo(3);    # says 'Got: 3'
+Foo->foo(12);   # dies

4265f531a53fd29c649ef6d1e866aba02a69c521
Toby Inkster <mail@tobyinkster.co.uk>
2013-12-27 08:56:20 +0000

    move strictures stuff later

--- lib/Moops/Keyword.pm
+++ lib/Moops/Keyword.pm
@@ -73,13 +73,13 @@ sub generate_package_setup
 		'use PerlX::Define;',
 		'use Scalar::Util qw(blessed);',
 		'use Try::Tiny;',
-		'use v5.14;',
-		'use strict;',
-		'use warnings FATAL => qw(all); no warnings qw(void once uninitialized numeric);',
 		'BEGIN { (*true, *false) = (\&Moops::_true, \&Moops::_false) };',
 		$self->generate_type_constraint_setup,
 		$self->generate_package_setup_oo,
 		$self->generate_package_setup_methods,
+		'use v5.14;',
+		'use strict;',
+		'use warnings FATAL => qw(all); no warnings qw(void once uninitialized numeric);',
 	);
 }
 

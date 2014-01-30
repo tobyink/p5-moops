9a2530a06879bf6853dadcd833e5f3c2608ca21e
Toby Inkster <mail@tobyinkster.co.uk>
2014-01-30 14:00:04 +0000

    clean up

--- examples/moops-as-app.pl
+++ examples/moops-as-app.pl
@@ -1,27 +1,25 @@
 #!/usr/bin/env perl
+
 use Moops;
 use Carp 'verbose';
 
-class myApp  {
-	use Moo;
+class UsrBinCat {
 	use MooX::Options;
-
-	option 'show_this_file' => (
+	
+	option filename => (
 		is       => 'ro',
 		format   => 's',
 		required => 1,
 		doc      => 'the file to display'
 	);
-
-	method BUILD {
-		open( my $fh, "<", $self->show_this_file ) or die  "cannot open < $self->show_this_file: $!";
-		while (<$fh>) {
-			print $_;
-		}
+	
+	method BUILD
+	{
+		open my $fh, "<", $self->filename
+			or die  "cannot open ${\ $self->filename }: $!";
+		
+		print while <$fh>;
 	}
 }
 
-#main has to come after myApp
-class main {
-	my $opt = myApp->new_with_options;
-}
+UsrBinCat->new_with_options;
